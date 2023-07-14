12d59e2a09b30000d837f34c8de84fa2ecd71a12
WenerSf3 <wsfonseca3@gmail.com>
2023-07-14 01:11:17 -0300

    feat

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000; // Porta na qual o servidor será executado
 
 app.listen(PORT, () => {
-    console.log(`API rodando na porta ${PORT}`);
-  });
-
-app.get('/', (req, res) => {
-  res.json({ title: "API em Node;js"}); // Exemplo de resposta do servidor
+  console.log(`API rodando na porta ${PORT}`);
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
