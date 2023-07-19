ad32d7bcf257175ae998dfc1e841283a75610bd8
WenerSf3 <wsfonseca3@gmail.com>
2023-07-18 22:27:32 -0300

    asdas

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/', (req, res) => {
 // login
 app.post('/login', async (req, res) => {
   const request = req.body;
-  
+
   try {
     const [account] = await database.query(
       'SELECT * FROM Users WHERE email = ? AND password = ? AND `key` = ? LIMIT 1',
@@ -56,12 +56,12 @@ app.post('/login', async (req, res) => {
 
     if (account.length !== 0) {
       browser = await playwright.firefox.launch({ headless: true });
-      if(browser){
+      if (browser) {
         status = true;
         res.status(200).json({ success: true, message: 'Logado!' });
       }
       res.status(404).json({ success: false, message: 'Erro no TTN' });
-  
+
     } else {
       res.status(404).json({ success: false, message: 'Usuário não encontrado' });
     }
@@ -73,107 +73,114 @@ app.post('/login', async (req, res) => {
 
 // Outras rotas (exemplo)
 app.get('/TTNstart', async (req, res) => {
-  if(!browser){
-    res.status(404).json({ error: 'Nao fez login' });
+  if (status == false) {
+    res.status(404).json({ error: 'esta deslogado' });
+  } else {
+    if (!browser) {
+      return
+    }else{
+      res.status(404).json({ error: 'Ja esta aberto' });
+    }
   }
-  if(page){
-    return;
-  }
-  page = await browser.newPage();
-  await page.goto('https://qxbroker.com/en/sign-in/');
-  await page.getByRole('textbox', { name: 'Email' }).fill('tradewener@gmail.com');
-  await page.getByRole('textbox', { name: 'Password' }).fill('wmgame9898');
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  await page.getByRole('button', { name: 'Sign in' }).click();
 
-  setTimeout(() => {
-    const verify = page.locator("#graph");
+    if (page) {
+      return;
+    }
+    page = await browser.newPage();
+    await page.goto('https://qxbroker.com/en/sign-in/');
+    await page.getByRole('textbox', { name: 'Email' }).fill('tradewener@gmail.com');
+    await page.getByRole('textbox', { name: 'Password' }).fill('wmgame9898');
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await page.getByRole('button', { name: 'Sign in' }).click();
 
-    let obg = {
-      msg: 'SIM Automação concluída com sucesso!',
-      success: true
-    }
-    let obg2 = {
-      msg: 'NAO Automação concluída com sucesso!',
-      success: false
-    }
-    if (verify) {
-      res.send(obg);
-    } else {
-      res.send(obg2);
-    }
-  }, 3000);
-});
+    setTimeout(() => {
+      const verify = page.locator("#graph");
+
+      let obg = {
+        msg: 'SIM Automação concluída com sucesso!',
+        success: true
+      }
+      let obg2 = {
+        msg: 'NAO Automação concluída com sucesso!',
+        success: false
+      }
+      if (verify) {
+        res.send(obg);
+      } else {
+        res.send(obg2);
+      }
+    }, 3000);
+  });
 
 app.post('/VerifyCode', async (req, res) => {
   const obj = req.body;
 
   await page.locator(".input-control-cabinet__input").fill(obj.codigo);
-  
+
   await page.locator(".button.button--primary.button--spaced").click();
 
   let chamouFuncao = false;
   await page.waitForNavigation({ timeout: 0 });
   let newUrl = page.url();
-  
+
   await new Promise(resolve => setTimeout(resolve, 3000));
   let i = 0;
   const interval = setInterval(async () => {
-      i += 1;
-    
-      if (newUrl != 'https://qxbroker.com/en/sign-in/' && !chamouFuncao) {
-          chamouFuncao = true;
-          clearInterval(interval);
-          res.send('LOGADO!');
-      }
+    i += 1;
 
-      if(i >6){
-        res.status(404).json('codigo incorreto');
-      }
-      
+    if (newUrl != 'https://qxbroker.com/en/sign-in/' && !chamouFuncao) {
+      chamouFuncao = true;
+      clearInterval(interval);
+      res.send('LOGADO!');
+    }
+
+    if (i > 6) {
+      res.status(404).json('codigo incorreto');
+    }
+
   }, 2000);
 
 });
 
 app.post('/TTNclose', (req, res) => {
-  if(status == false){
-    res.status(404).json({Ttn:false,message:'TTN está fechado'});
+  if (status == false) {
+    res.status(404).json({ Ttn: false, message: 'TTN está fechado' });
   }
-    page.close();
+  page.close();
 
-    res.send('Ttn fechado');
+  res.send('Ttn fechado');
 });
 
 app.post('/logout', (req, res) => {
-  if(status == false){
-    if(browser){
+  if (status == false) {
+    if (browser) {
       browser.close();
     }
-    res.status(404).json({Ttn:false,message:'TTN está fechado'});
+    res.status(404).json({ Ttn: false, message: 'TTN está fechado' });
   }
-    browser.close();
-    status = false;
+  browser.close();
+  status = false;
 
-    res.send('Ttn fechado');
+  res.send('Ttn fechado');
 });
 
 app.post('/Call', async (req, res) => {
-    if(status == false){
-      res.status(404).json({Ttn:false,message:'TTN está fechado'});
-    }
-    await page.locator(".button.button--success.button--spaced.call-btn.section-deal__button").click();
+  if (status == false) {
+    res.status(404).json({ Ttn: false, message: 'TTN está fechado' });
+  }
+  await page.locator(".button.button--success.button--spaced.call-btn.section-deal__button").click();
 
-    res.send('compra executada');
+  res.send('compra executada');
 
 });
 
 app.post('/Put', async (req, res) => {
-    if(status == false){
-      res.status(404).json({Ttn:false,message:'TTN está fechado'});
-    }
-    await page.locator(".button.button--danger.button--spaced.put-btn.section-deal__button").click();
-    
-    res.send('Venda executada');
+  if (status == false) {
+    res.status(404).json({ Ttn: false, message: 'TTN está fechado' });
+  }
+  await page.locator(".button.button--danger.button--spaced.put-btn.section-deal__button").click();
+
+  res.send('Venda executada');
 
 });
 
@@ -199,15 +206,15 @@ app.post('/hors', async (req, res) => {
 app.post('/AlterCambio', async (req, res) => {
   const obj = req.body;
 
-  if(status == false){
-    res.status(404).json({Ttn:false,message:'TTN está fechado'});
+  if (status == false) {
+    res.status(404).json({ Ttn: false, message: 'TTN está fechado' });
   }
   await page.locator(".asset-select__button").click();
   await page.waitForTimeout(1000);
   await page.locator(".asset-select__search-input").fill(String(obj.ativo));
   await page.waitForTimeout(1000);
   await page.locator(".assets-table__name").click();
-  
+
   res.send('Ativo trocado com sucesso!');
 
 });
