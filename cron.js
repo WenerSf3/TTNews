a4b7e46375b5e465c74927908397dc045aa19616
WenerSf3 <wsfonseca3@gmail.com>
2023-07-31 23:44:29 -0300

    sadasd

--- cron.js
+++ cron.js
@@ -1,32 +1,30 @@
-const connection = require("./connection.js");
-const CronJob = require('cron').CronJob;
+var CronJob = require('cron').CronJob;
 const axios = require('axios');
+const connection = require("./config/connection");
+let database = connection.promise();
 require('dotenv').config();
 
 const IP = process.env.IP_LOCAL || process.env.IP_PRODUCT;
 const PORT = process.env.PORT;
 
-const database = connection.promise();
-
-const job = new CronJob(
-    '*/5 * * * *',
+var job = new CronJob(
+    '* * * * * *',
     async function () {
-        axios.get('https://webhook.site/3abc192f-c0a6-40f9-bb3e-3f017251bc2d');
+        await axios.get('https://webhook.site/3abc192f-c0a6-40f9-bb3e-3f017251bc2d');
         try {
             const [event] = await database.query(
                 `SELECT * FROM Eventos WHERE posicao = 'pendente' ORDER BY ABS(TIMESTAMPDIFF(SECOND, date, NOW())) DESC;`
             );
             const [account] = await database.query(`SELECT * FROM Users LIMIT 1;`);
-
+                console.log(`http://${IP}:${PORT}/preparingEvent`)
             if (event.length > 0 && account.search !== '0') {
                 let obj = {
                     argument: 'start'
                 };
-                try {
-                    await axios.post(`http://${IP}:${PORT}/preparingEvent`, obj);
-                } catch (error) {
-                    return;
-                }
+                const test = await axios.post(`http://${IP}:${PORT}/preparingEvent`, obj);
+                console.log(test);
+
+
             } else {
                 console.log('Nenhum evento Próximo');
             }
@@ -39,5 +37,4 @@ const job = new CronJob(
     true,
     'America/Los_Angeles'
 );
-
 job.start();
