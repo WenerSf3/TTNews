8cd8829105b718a67d7d6f9f48c26a3b601caf04
WenerSf3 <wsfonseca3@gmail.com>
2023-09-03 21:48:15 -0300

    asdas

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 // Configuração da conexão
 const connection = mysql.createConnection({
     host: '154.56.41.121',
-    user: 'wener2',
+    user: 'wener',
     password: '@123Mudar!',
     database: 'ttnBot',
     port: 3306
