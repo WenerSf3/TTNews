a8a799cbbd90cdf8bb27e89a638a1d1b9239e0c1
WenerSf3 <wsfonseca3@gmail.com>
2023-09-05 20:21:23 -0300

    dasdsad

--- config/startEvent.js
+++ config/startEvent.js
@@ -25,7 +25,7 @@ async function startEvent(evento, web) {
   let time = setInterval(async () => {
     horarioMoment = horarioMoment.add(1, "second");
     missing = hourEvent.diff(horarioMoment, "seconds");
-    if (missing <= 27 && !started) {
+    if (missing <= 40 && !started) {
       started = true;
       global.price = await getActive(evento.active);
       clearInterval(time);
