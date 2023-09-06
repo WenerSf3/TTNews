cfb9ac16b3ba46650d766403b8b8e1de528f093d
WenerSf3 <wsfonseca3@gmail.com>
2023-09-05 23:37:37 -0300

    feasd

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.post('/preparingEvent', (req, res) => {
 
 app.get('/cron', async (req, res) => {
   const [events] = await database.query(
-    `SELECT * FROM cron;`
+    `SELECT * FROM cron ORDER BY id DESC LIMIT 10;`
   );
   return res.status(200).json({ success: true, cron:events });
 
